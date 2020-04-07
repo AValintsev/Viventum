b1ae013a521fe1bdc230d6f6e2cde8d7b67fef1e
Anton Valintsev <valintsev.anton@gmail.com>
2020-04-07 21:15:59 +0300

    first commit

---
+++ Viventum/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Viventum.Models;
+
+namespace Viventum.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult ContactForm()
+        {
+            return PartialView(new ContactModel());
+        }
+
+        [HttpPost]
+        public PartialViewResult ContactForm(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var c = model.Company;
+                var e = model.Email;
+                var m = model.Message;
+
+                // Send an email
+                // Save to file
+
+            }
+
+            return PartialView(new ContactModel());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Viventum/Models/ContactModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Viventum.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string Company { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string Subject { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string Message { get; set; }
+
+    }
+}
